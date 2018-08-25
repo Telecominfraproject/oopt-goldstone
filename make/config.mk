2e00753bf992408b26c00c59fe957d55a5d9a4dd
Wataru Ishida <ishida@nel-america.com>
2018-08-24 18:49:46 -0700

    add x1-sonic package
    basic files for booting up SONiC
    
    Signed-off-by: Wataru Ishida <ishida@nel-america.com>

--- make/config.mk
+++ make/config.mk
@@ -2,3 +2,10 @@ include $(ONL)/make/config.mk
 #
 # Custom configuration here
 #
+ifndef SONIC
+    export SONIC := $(X1)/sm/sonic-buildimage
+endif
+
+ifndef sonicadmin_user
+    export sonicadmin_user := root
+endif
