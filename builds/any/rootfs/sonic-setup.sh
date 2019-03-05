3c1d2793a776982897a0a01669626d70737548ea
Carl D. Roth <carl.roth@bigswitch.com>
2019-03-05 13:54:02 -0800

    missing directory

--- builds/any/rootfs/sonic-setup.sh
+++ builds/any/rootfs/sonic-setup.sh
@@ -20,3 +20,7 @@ DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get update
 
 DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y install python-click-default-group python-tabulate python-natsort
 # get these packages once sonic-dev is enabled (after initial multistrap)
+
+# missing files from the sonic installer
+# (see # sonic-buildimage/files/build_templates/sonic_debian_extension.j2)
+mkdir -p $FILESYSTEM_ROOT/var/cache/sonic
