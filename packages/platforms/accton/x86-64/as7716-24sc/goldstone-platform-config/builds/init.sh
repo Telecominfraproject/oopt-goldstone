d83eceb1c6550192b525fd0d49db3dc13a6373ff
Wataru Ishida <wataru.ishid@gmail.com>
2022-09-21 22:05:57 +0900

    cassini: fix transponder support
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

--- packages/platforms/accton/x86-64/as7716-24sc/goldstone-platform-config/builds/init.sh
+++ packages/platforms/accton/x86-64/as7716-24sc/goldstone-platform-config/builds/init.sh
@@ -2,6 +2,20 @@
 
 set -eux
 
+modprobe cfp2piu
+
+echo piu1 0x6A > /sys/bus/i2c/devices/i2c-41/new_device
+echo piu2 0x6A > /sys/bus/i2c/devices/i2c-43/new_device
+echo piu3 0x6A > /sys/bus/i2c/devices/i2c-45/new_device
+echo piu4 0x6A > /sys/bus/i2c/devices/i2c-47/new_device
+echo piu5 0x6A > /sys/bus/i2c/devices/i2c-49/new_device
+echo piu6 0x6A > /sys/bus/i2c/devices/i2c-51/new_device
+echo piu7 0x6A > /sys/bus/i2c/devices/i2c-53/new_device
+echo piu8 0x6A > /sys/bus/i2c/devices/i2c-55/new_device
+
+systemctl disable ipmi
+systemctl disable ipmievd
+
 systemctl enable gs-south-onlp
 systemctl enable gs-south-tai
 systemctl enable gs-south-system
