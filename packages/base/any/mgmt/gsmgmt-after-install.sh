99f170bb7e807f8fa22091d32dedce26ec08b72e
Noguchi Ko <92015366+noguchiko@users.noreply.github.com>
2022-11-15 10:13:32 +0900

    mgmt: add gs-mgmt-system.target
    This change adds a new target gs-mgmt-system to the mgmt package. The
    target is for system daemons.

--- packages/base/any/mgmt/gsmgmt-after-install.sh
+++ packages/base/any/mgmt/gsmgmt-after-install.sh
@@ -2,4 +2,4 @@
 
 set -eux
 
-systemctl enable gs-mgmt.target gs-mgmt-south.target gs-mgmt-north.target gs-mgmt-xlate.target gs-north-notif.service
+systemctl enable gs-mgmt.target gs-mgmt-south.target gs-mgmt-north.target gs-mgmt-xlate.target gs-mgmt-system.target gs-north-notif.service
--- packages/base/any/mgmt/gsmgmt-before-remove.sh
+++ packages/base/any/mgmt/gsmgmt-before-remove.sh
@@ -2,5 +2,5 @@
 
 set -eux
 
-systemctl disable gs-mgmt.target gs-mgmt-south.target gs-mgmt-north.target gs-mgmt-xlate.target gs-north-notif.service
-systemctl stop gs-mgmt.target gs-mgmt-south.target gs-mgmt-north.target gs-mgmt-xlate.target gs-north-notif.service
+systemctl disable gs-mgmt.target gs-mgmt-south.target gs-mgmt-north.target gs-mgmt-xlate.target gs-mgmt-system.target gs-north-notif.service
+systemctl stop gs-mgmt.target gs-mgmt-south.target gs-mgmt-north.target gs-mgmt-xlate.target gs-mgmt-system.target gs-north-notif.service
