7ca373a39666934c5cc602e530cae1ec9fcc1965
Tatsuya Matsuo <96454784+matsuo-tat@users.noreply.github.com>
2023-03-24 16:10:52 +0900

    as7946-30xb: add files and startup process for xcvrd

--- packages/platforms/accton/x86-64/as7946-30xb/goldstone-platform-config/builds/init.sh
+++ packages/platforms/accton/x86-64/as7946-30xb/goldstone-platform-config/builds/init.sh
@@ -2,6 +2,10 @@
 
 set -eux
 
+# release reset states of QSFP modules
+echo 0 | tee $(seq -f "/sys/devices/pci0000:00/0000:00:1f.3/i2c-0/i2c-1/i2c-12/12-0061/module_reset_%g" 1 16) 1> /dev/null
+echo 0 | tee $(seq -f "/sys/devices/pci0000:00/0000:00:1f.3/i2c-0/i2c-1/i2c-13/13-0062/module_reset_%g" 17 26) 1> /dev/null
+
 systemctl disable usonic
 systemctl disable tai
 systemctl disable gs-mgmt-xlate.target
@@ -10,3 +14,4 @@ systemctl disable gs-north-gnmi
 systemctl enable ocnos
 systemctl enable gs-south-onlp
 systemctl enable gs-south-ocnos
+systemctl enable xcvrd
