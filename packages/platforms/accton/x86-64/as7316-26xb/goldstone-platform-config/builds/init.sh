18f2ff9e2b8ed1299ff077a7ea704adc3bc307a3
KazuyaAnazawa <m5201120.grd@gmail.com>
2023-07-24 14:54:33 +0900

    as7316-26xb: enabled south target and south-onlp

--- packages/platforms/accton/x86-64/as7316-26xb/goldstone-platform-config/builds/init.sh
+++ packages/platforms/accton/x86-64/as7316-26xb/goldstone-platform-config/builds/init.sh
@@ -5,6 +5,7 @@ set -eux
 systemctl disable usonic
 systemctl disable tai
 systemctl disable gs-mgmt-xlate.target
-systemctl disable gs-mgmt-south.target
+systemctl enable gs-mgmt-south.target
 systemctl disable gs-north-gnmi
 systemctl enable ocnos
+systemctl enable gs-south-onlp
